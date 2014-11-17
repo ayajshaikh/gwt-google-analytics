7c7e8b5f1aabb0ada2f45b0d79e9ea170ce7e355
ayajshaikh <ayajshaikh@gmail.com>
2014-11-17 18:55:14 +0530

    Added JavaDocs Comments

--- src/main/java/com/alignace/gwt/ganalytics/client/GAnalytics.java
+++ src/main/java/com/alignace/gwt/ganalytics/client/GAnalytics.java
@@ -3,25 +3,50 @@ package com.alignace.gwt.ganalytics.client;
 import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.ScriptInjector;
 
+/**
+ * GWT - Google Analytics
+ * 
+ * @author Ayajahmed Shaikh
+ * @version 1.0
+ */
 public class GAnalytics {
 	private static final String GOOGLE_ANALYTICS_JAVASCRIPT_URL = "https://www.google-analytics.com/ga.js";
 
+	/**
+	 * Constructor is private just to avoid accidental use without
+	 * initialization
+	 */
 	private GAnalytics() {
 
 	}
 
+	/**
+	 * This should always be called first to initialize Google Analytics.
+	 * 
+	 * @param trackingId - Tracking Id of Google Analytics (e.g. UA-XXXXXXXX-X)
+	 * @param callback
+	 */
 	public static void initialize(String trackingId,
 			Callback<Void, Exception> callback) {
 		setAnalyticsParams(trackingId);
 		inject(callback);
 	}
 
+	/**
+	 * Native Method to initialize
+	 * @param trackingId
+	 * @return
+	 */
 	private static native String setAnalyticsParams(String trackingId) /*-{
 		var _gaq = _gaq || [];
 		_gaq.push([ '_setAccount', trackingId ]);
 		_gaq.push([ '_trackPageview' ]);
 	}-*/;
 
+	/**
+	 * Injects Google Analytics Javascript
+	 * @param callback
+	 */
 	private static void inject(final Callback<Void, Exception> callback) {
 		if (!isInjected()) {
 			ScriptInjector.fromUrl(GOOGLE_ANALYTICS_JAVASCRIPT_URL)
@@ -39,7 +64,11 @@ public class GAnalytics {
 		}
 	}
 
+	/**
+	 * Checks if GA JS is already imported
+	 * @return
+	 */
 	private static native boolean isInjected() /*-{
-												return typeof $wnd._gat !== "undefined";
-												}-*/;
+		return typeof $wnd._gat !== "undefined";
+	}-*/;
 }
